49dd53dddb2f30ca3a62d0edff81eed7dbe8eb1c
hgowton <hgowton@gmail.com>
2020-01-13 20:56:36 -0500

    Using array wins, guesses left, and loses correctly talley.  Need to fix: second go around after win and determine how letter guessed display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,41 @@
 var psychicGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+//Create variables to hold number of wins, loses, guesses left, and letters guessed
+
+// NEW NEWvar userGuess = '';
+// var userWins = 1;
+// var userLosses = 0;
+// var totalGuess = 9;
 var userGuess = '';
-var wins = 1; 
-var loses = 0;
+var wins = 0;
+var losses = 0;
 var totalGuess = 9;
+
 var compGuess = '';
 
 
 //var psychicLetter should not appear on final project - shows computer's guess
 var psychicLetter = document.getElementById("psychicLetter");
 
-var userGuess = document.getElementById("userGuess");
-var displayGuessLeft = document.getElementById("totalGuess");
-var displayWins = document.getElementById("wins");
-var displayLoses = document.getElementById("loses");
+//Array to hold user guesses
+var userGuessSoFar = [];
+
+//ATTEMPT2 to display userarray on screen
+var displayGuess = document.getElementById("userGuessSoFar")
+
+//ATTEMPT1 to display userarray on screen
+// var userGuessSoFarDisplay = document.getElementById[userGuessSoFar];
+// var useGuessString = userGuessSoFar.toString();
+
+// NEWNEW
+// var displayGuessLeft = document.getElementById("totalGuess");
+// var displayWins = document.getElementById("wins");
+// var displayLoses = document.getElementById("loses");
+
+//Variables to Hold references to the places in the HTML where we want to display things
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var totalGuessText = document.getElementById("guesses-text")
 
 
 //This funciton selects a random letter from my Psychic array
@@ -22,43 +44,65 @@ function getPsychicLetter () {
     //Pics random index value from psychicGuess array
     //Writes chosen value to HTML
     psychicLetter.textContent = psychicGuess[random];
-    
+
     console.log("Computer's Choice: " + psychicGuess[random]);
     return psychicGuess[random];
 }
 
 compGuess = getPsychicLetter();
 
-document.onkeyup = function(event) {
-    // Determines which key was pressed and adds letter to already guessed field on screen
-    userGuess.textContent += event.key + ", ";
-    userGuess2 = event.key;
-   
-    for (i=0; i < compGuess.length; i++){
 
-        if(userGuess2 === compGuess[i]){
-            console.log("Yes " + compGuess[i]);
+
+document.onkeyup = function(event) {
+    // Determines which key was pressed.
+    userGuess = event.key;
+    userGuessSoFar.push(userGuess); 
+
+    // attempt to get array to display on screen
+    // for(i=0; i<userGuessSoFar.length; i++) {
+    //     document.getElementById("userGuess").innerHTML;
+    // }
+
+    for (j=0; j < compGuess.length; j++){
+
+        if(userGuess === compGuess[j]){
+            console.log(compGuess[j]);
             alert("You guessed right!")
-            displayWins.textContent = "wins: " +wins++;
-            displayGuessLeft.textContent = + 9;
+            //+1 wins
+            // displayWins.textContent = "Wins: " +1;
+            wins++;
+            totalGuess = 9;
+            userGuessSoFar = [];
             getPsychicLetter();
+            //reset the guess counter
+            //reset the guess array
         }
         else {
-            console.log(userGuess);
-            displayGuessLeft.textContent = -1;
-
+            // console.log(userGuess);
+            console.log(userGuessSoFar);
+            // displayLoses.textContent = "Loses: " +1;
+            // displayGuessLeft.textContent -1;
+            // losses++;
+            totalGuess--;
         }  
 
     }
 
-    if (totalGuess = 0) {
-        displayLoses.textContent = "loses: " +loses++;
-        displayGuessLeft.textContent --;
-
-        // displayGuessLeft.textContent = "Total Guesses Left: " + totalGuess + 9;
+    if(totalGuess === 0){
+        losses++;
+        totalGuess = 9;
+        userGuessSoFar = [];
         getPsychicLetter();
+        alert("You Lost")
     }
-}
+
+   //Display to screen guesses, wins, loses, guesses left
+   winsText.textContent = "wins: " + wins;
+   lossesText.textContent = "losses: " + losses;
+   totalGuessText.textContent = "guesses left: " + totalGuess;
+//    ATTEMPT3 display array
+//    userGuessText.textContnet = "letters guessed; " + userGuess;
+
+}; 
 
 
-    console.log(compGuess);
