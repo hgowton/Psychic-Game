9355c16916b3002ddda7443b11506c30f69f617f
hgowton <hgowton@gmail.com>
2020-01-14 22:41:38 -0500

    removed computer guess from game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,6 @@
 var psychicGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //Create variables to hold number of wins, loses, guesses left, and letters guessed
-
-// NEW NEWvar userGuess = '';
-// var userWins = 1;
-// var userLosses = 0;
-// var totalGuess = 9;
 var userGuess = '';
 var wins = 0;
 var losses = 0;
@@ -15,37 +10,19 @@ var compGuess = '';
 
 //var psychicLetter should not appear on final project - shows computer's guess
 var psychicLetter = document.getElementById("psychicLetter");
-
-//Array to hold user guesses
 var userGuessSoFar = [];
-
-//ATTEMPT2 to display userarray on screen
 var displayGuess = document.getElementById("userGuessSoFar")
-
-//ATTEMPT1 to display userarray on screen
-// var userGuessSoFarDisplay = document.getElementById[userGuessSoFar];
-// var useGuessString = userGuessSoFar.toString();
-
-// NEWNEW
-// var displayGuessLeft = document.getElementById("totalGuess");
-// var displayWins = document.getElementById("wins");
-// var displayLoses = document.getElementById("loses");
 
 //Variables to Hold references to the places in the HTML where we want to display things
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var totalGuessText = document.getElementById("guesses-text");
-//NEWNEW attempt of psychic string
-var PsychicString = document.getElementById("psychicstring");
-
 
 //This funciton selects a random letter from my Psychic array
 function getPsychicLetter () {
     var random = Math.floor(Math.random() *(psychicGuess.length-1));
+    
     //Pics random index value from psychicGuess array
-    //Writes chosen value to HTML
-    psychicLetter.textContent = psychicGuess[random];
-
     console.log("Computer's Choice: " + psychicGuess[random]);
     return psychicGuess[random];
 }
